ebc91f26c7b69288c51eac2a2fb295c24eff4677
Vinayaka Bhat <vinayaka.bhat@sclera.com>
2025-07-25 14:24:14 +0530

    first ever dockerfile with omhttp is added here

--- monitor.sh
+++ monitor.sh
@@ -3,11 +3,10 @@
 ./monitor -i $NETWORK_ADDRESS -m -C -D -v $AGENT_ID -n $DOCKER_ID &
 /bin/bash docker_app_runner.sh &
 /bin/bash firewall_rules.sh &
-/etc/init.d/dbus start &
-/etc/init.d/avahi-daemon start &
-#echo "Starting socat UDP Log forwarder..."
-#/usr/bin/socat -v UDP4-RECVFROM:514,fork UDP4-SENDTO:scleravdmsnetworkgateway:8888 &
-#sleep 2
-/etc/rsyslogd -n -f /etc/rsyslog.conf &
-java -jar sclera_docker.jar &
-#tail -f /dev/null
+/etc/init.d/dbus start
+/etc/init.d/avahi-daemon start
+rsyslogd -n -f /etc/rsyslog.conf &
+java -jar sclera_docker.jar
+
+
+
